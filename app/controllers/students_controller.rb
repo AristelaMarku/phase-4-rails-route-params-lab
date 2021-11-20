63ccd0f8c96b248c5424886f721b38edea500e9c
Aristela Marku <aristela.marku@gmail.com>
2021-11-21 00:03:46 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,24 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students=Student.all
     render json: students
+end
+
+def grades
+  students=Student.all.order(grade: :desc)
+  render json: students
+end
+
+def highest_grade
+ students=Student.all.order(grade: :desc).first
+ render json: students
+end
+
+  def show
+   onestudent=Student.find(params[:id])
+   render json: onestudent
   end
+
 
 end
